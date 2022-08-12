c8f80b9ff254f22a22e41e10255981f228b87336
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-12 17:08:02 +0500

    add constants for database

---
+++ app/src/main/java/xyz/teamgravity/roomtrigger/data/local/constant/NumberConst.kt
@@ -0,0 +1,28 @@
+package xyz.teamgravity.roomtrigger.data.local.constant
+
+object NumberConst {
+
+    /**
+     * Database name
+     */
+    const val NAME = "database_number"
+
+    /**
+     * Database version
+     */
+    const val VERSION = 1
+
+    /**
+     * Table number
+     * Entity -> [xyz.teamgravity.roomtrigger.data.local.entity.NumberEntity]
+     * Model -> [xyz.teamgravity.roomtrigger.data.model.NumberModel]
+     */
+    const val TABLE_NUMBER = "table_number"
+
+    /**
+     * Table count
+     * Entity -> [xyz.teamgravity.roomtrigger.data.local.entity.CountEntity]
+     * Model -> [xyz.teamgravity.roomtrigger.data.model.CountModel]
+     */
+    const val TABLE_COUNT = "table_count"
+}
